4fd52477cd305231c55d5a95b821b5d00d22be9b
nguyentrunghieu26072004-create <nguyentrunghieu26072004@gmail.com>
2026-05-22 13:59:38 +0700

    Delete vd1.py

--- vd1.py
+++
@@ -1,9 +0,0 @@
-m = int(input("Nhap m: "))
-n = int(input("Nhap n: "))
-tong = m + n
-lon_nhat = 0
-for ky_tu in str(tong):
-    if int(ky_tu) > lon_nhat:
-        lon_nhat = int(ky_tu)
-print(f"Tong: {tong}")
-print(f"Chu so lon nhat: {lon_nhat}")
